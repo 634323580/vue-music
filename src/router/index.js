e94d47cb5ce052ba54ca12a9b73d43244949adf2
ChenZhiPeng <634323580@qq.com>
2017-09-22 18:40:47 +0800

    去除按需加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,46 +1,46 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// // import Home from '@/view/home'
-// import Music from '@/view/music'
-// import Search from '@/view/search/search'
-// import Lately from '@/view/lately/lately'
-// import Ranking from '@/view/ranking'
-// import Toplist from '@/view/toplist'
+// import Home from '@/view/home'
+import Music from '@/view/music'
+import Search from '@/view/search/search'
+import Lately from '@/view/lately/lately'
+import Ranking from '@/view/ranking'
+import Toplist from '@/view/toplist'
 
 Vue.use(Router)
-
-// export default new Router({
-//   linkActiveClass: 'on',
-//   routes: [
-//     { name: 'music', path: '/music', component: Music },
-//     { name: 'search', path: '/search', component: Search },
-//     { name: 'lately', path: '/lately', component: Lately },
-//     { 
-//       name: 'ranking', 
-//       path: '/ranking', 
-//       component: Ranking,
-//       children: [
-//         {name: 'toplist', path: ':type', component: Toplist}
-//       ]
-//     },
-//     { path: '/*', redirect: '/music' }
-//   ]
-// })
 
 export default new Router({
   linkActiveClass: 'on',
   routes: [
-    { name: 'music', path: '/music', component: resolve => require(['@/view/music'], resolve) },
-    { name: 'search', path: '/search', component: resolve => require(['@/view/search/search'], resolve) },
-    { name: 'lately', path: '/lately', component: resolve => require(['@/view/lately/lately'], resolve) },
+    { name: 'music', path: '/music', component: Music },
+    { name: 'search', path: '/search', component: Search },
+    { name: 'lately', path: '/lately', component: Lately },
     { 
       name: 'ranking', 
       path: '/ranking', 
-      component: resolve => require(['@/view/ranking'], resolve),
+      component: Ranking,
       children: [
-        {name: 'toplist', path: ':type', component: resolve => require(['@/view/toplist'], resolve)}
+        {name: 'toplist', path: ':type', component: Toplist}
       ]
     },
     { path: '/*', redirect: '/music' }
   ]
 })
+
+// export default new Router({
+//   linkActiveClass: 'on',
+//   routes: [
+//     { name: 'music', path: '/music', component: resolve => require(['@/view/music'], resolve) },
+//     { name: 'search', path: '/search', component: resolve => require(['@/view/search/search'], resolve) },
+//     { name: 'lately', path: '/lately', component: resolve => require(['@/view/lately/lately'], resolve) },
+//     { 
+//       name: 'ranking', 
+//       path: '/ranking', 
+//       component: resolve => require(['@/view/ranking'], resolve),
+//       children: [
+//         {name: 'toplist', path: ':type', component: resolve => require(['@/view/toplist'], resolve)}
+//       ]
+//     },
+//     { path: '/*', redirect: '/music' }
+//   ]
+// })
